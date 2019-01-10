aa052db5ad7e6afd4b9f811ba5f42532e5eea7a4
Karol Harumi <karol.harumi@hotmail.com>
2019-01-10 18:49:23 +1000

    [General] project init

---
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,16 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+
+    }
+
+    obtemVolume() {
+
+        return this.quantidade * this.valor;
+    }
+
+}
