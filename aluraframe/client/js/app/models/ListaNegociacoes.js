016d0ee625b14d2c62b10db225d7471f9ad0f045
Karol Harumi <karol.harumi@hotmail.com>
2019-02-03 14:54:24 +1000

    [General] learning factory and bind

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,33 +1,26 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
+    constructor() {
 
         this._negociacoes = [];
-        this._armadilha = armadilha;
-        
     }
 
     adiciona(negociacao) {
-
-        this._negociacoes.push(negociacao);
-        this._armadilha(this);
+        
+        this._negociacoes = [].concat(this._negociacoes, negociacao);
+        //this._negociacoes.push(negociacao);
         //Reflect.apply(this._armadilha, this._contexto, [this]);
-
-    }
-
-    esvazia() {
-
-        this._negociacoes = [];
-        this._armadilha(this);
-        //Reflect.apply(this._armadilha, this._contexto, [this]);
-
     }
 
     get negociacoes() {
-
         // truque: programacao defensiva: se alguem tentar alterar minha lista, vai alterar apenas a copia dela
         return [].concat(this._negociacoes);
-
     }
+
+    esvazia() {
+        this._negociacoes = [];
+        //Reflect.apply(this._armadilha, this._contexto, [this]);
+    }
+
 
 }
